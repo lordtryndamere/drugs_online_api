e28e04c06c398bdcb743ae8bc3585c8e6d57ce4d
unknown <kevin.feo@educar.com.co>
2020-10-11 14:19:12 -0500

    fixeds name

--- services/validation.js
+++ services/validation.js
@@ -4,7 +4,7 @@ const Joi = require('@hapi/joi');
 
 const registervalidation = data =>{
     const schema  = {
-        name:Joi.string().min(6).required(),
+        name:Joi.string().min(3).required(),
         email:Joi.string().min(6).required().email(),
         password:Joi.string().min(6).required(),
         phone:Joi.number().min(10).required(),
