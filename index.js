c95915a62e2e3b8d374e596d10d546b834ffd1cd
unknown <kevin.feo@educar.com.co>
2020-10-11 17:31:48 -0500

    fixeds headers

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(helmet());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials');
-    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY,autorization, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Headers', 'auth-token,Authorization, X-API-KEY,autorization, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
