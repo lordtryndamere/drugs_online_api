57ac3d6e37af2407abbb5e515134cf3b83b8795c
unknown <kevin.feo@educar.com.co>
2020-10-11 11:02:10 -0500

    fixeds messanges errors and success

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,11 +15,11 @@ class AuthController {
 
     //Checking if the  email exists
     const user = await User.findOne({ email: item.email });
-    if (!user) return res.status(400).send({ code:400, message:"Email is not found"});
+    if (!user) return res.status(400).send({ code:400, message:"No se encontro este email"});
     //Password is correct
 
     const validPass = await bcrypt.compare(item.password, user.password);
-    if (!validPass) return res.status(400).send({ code:400, message:"invalid password"});
+    if (!validPass) return res.status(400).send({ code:400, message:"Contraseña incorrecta"});
 
     //Create and assign a token
 
@@ -42,7 +42,7 @@ class AuthController {
     
     //Checking if the user is already in the database
     const emailExist = await User.findOne({ email: item.email });
-    if (emailExist) return res.status(400).send({ code:400, message:"Email ya existe"});
+    if (emailExist) return res.status(400).send({ code:400, message:"Este email ya esta registrado"});
     //Hash password
     const salt = await bcrypt.genSalt(10);
     const hassPassword = await bcrypt.hash(item.password, salt);
@@ -112,7 +112,7 @@ class AuthController {
       .status(200)
       .send({
         code: 200,
-        message: "Usuarios encontrado exitosamente",
+        message: "Usuarios encontrados exitosamente",
         users: findUser,
       });
   };
